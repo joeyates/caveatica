081eb892770b00e94c555e060b32a6480478bc36
Joe Yates <joe.g.yates@gmail.com>
2023-12-24 14:41:32 +0100

    Update locking logic

--- lib/caveatica/camera.ex
+++ lib/caveatica/camera.ex
@@ -5,7 +5,7 @@ defmodule Caveatica.Camera do
   @name :camera
   @photo_interval 1000 # ms
   @upload_path to_charlist("/home/dokku/caveatica/data/caveatica.jpg")
-  @lock_path to_charlist("/home/dokku/caveatica/data/caveatica.lock")
+  @photo_ready_lock_path to_charlist("/home/dokku/caveatica/data/caveatica.lock")
 
   def start_link(_opts) do
     Logger.info "Caveatica.Camera.start_link/1"
@@ -17,49 +17,49 @@ defmodule Caveatica.Camera do
     Logger.info "Caveatica.Camera.init/1"
 
     # Give Connection time to start
-    Process.send_after(self(), :take_photo, @photo_interval)
+    Process.send_after(self(), :upload_photo, @photo_interval)
 
     {:ok, nil}
   end
 
   @impl true
-  def handle_info(:take_photo, state) do
-    Logger.info "Caveatica.Camera.handle_info `:take_photo`"
+  def handle_info(:upload_photo, state) do
+    Logger.info "Caveatica.Camera checking whther to upload photo"
     pid = Process.whereis(:connection)
-    {:message_queue_len, length} = Process.info(pid, :message_queue_len)
-    Logger.info "Caveatica.Camera.handle_info, Connection message queue length: #{length}"
-    if length == 0 do
-      lock_exists = lock_exists()
-      Logger.info("Caveatica.Camera.handle_info, lock_exists: #{lock_exists}")
-      if !lock_exists do
-        take_photo()
-        create_lock()
-      end
+    with {:message_queue_len, 0} <- Process.info(pid, :message_queue_len),
+         %{status: :connected} <- Caveatica.Connection.status(),
+         false <- lock_exists() do
+      upload_photo()
+      create_lock()
+    else
+      _ ->
+        Logger.info "Caveatica.Camera skipping photo upload"
     end
 
-    Process.send_after(self(), :take_photo, @photo_interval)
+    Process.send_after(self(), :upload_photo, @photo_interval)
 
     {:noreply, state}
   end
 
   defp create_lock do
-    result = GenServer.call(:connection, {:send_binary, %{binary: "lock", pathname: @lock_path}}, :infinity)
-    Logger.info "Caveatica.Camera.create_lock/0 send_binary result: #{inspect(result, [pretty: true, width: 0])}"
+    result = GenServer.call(:connection, {:send_binary, %{binary: "lock", pathname: @photo_ready_lock_path}}, :infinity)
+    Logger.info "Caveatica.Camera.create_lock/0 result: #{inspect(result, [pretty: true, width: 0])}"
   end
 
   defp lock_exists do
-    case GenServer.call(:connection, {:file_info, @lock_path}, :infinity) do
-      {:ok, info} ->
-        Logger.info("Caveatica.Camera.lock_exists info: #{inspect(info, [pretty: true, width: 0])}")
+    case GenServer.call(:connection, {:file_info, @photo_ready_lock_path}, :infinity) do
+      {:ok, _info} ->
+        Logger.info("Caveatica.Camera photo ready lock exists on server")
         true
       _ ->
-        Logger.info("Caveatica.Camera.lock_exists doesn't exist")
+        Logger.info("Caveatica.Camera photo ready lock doesn't exist on server")
         false
     end
   end
 
-  defp take_photo do
+  defp upload_photo do
+    Logger.info "Caveatica.Camera uploading photo`"
     result = GenServer.call(:connection, {:send_binary, %{binary: Picam.next_frame(), pathname: @upload_path}}, :infinity)
-    Logger.info "Caveatica.Camera.take_photo/0 send_binary result: #{inspect(result, [pretty: true, width: 0])}"
+    Logger.info "Caveatica.Camera.upload_photo/0 result: #{inspect(result, [pretty: true, width: 0])}"
   end
 end
