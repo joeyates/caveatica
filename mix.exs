5347effa3d35064998f4eca781bd54d0406b8237
Joe Yates <joe.g.yates@gmail.com>
2024-01-02 11:36:44 +0100

    Update Erlang and Elixir

--- mix.exs
+++ mix.exs
@@ -29,13 +29,13 @@ defmodule Caveatica.MixProject do
   defp deps do
     [
       {:circuits_gpio, "~> 1.0"},
-      {:nerves, "~> 1.9.0", runtime: false},
+      {:nerves, "~> 1.10.0", runtime: false},
       {:nerves_time, "~> 0.4.2", targets: @target},
-      {:nerves_runtime, "~> 0.13.0", targets: @target},
+      {:nerves_runtime, "~> 0.13.6", targets: @target},
       {:nerves_pack, "~> 0.7.0", targets: @target},
-      {:nerves_system_rpi0, "~> 1.22", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi3, "~> 1.22", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi4, "~> 1.22", runtime: false, targets: :rpi4},
+      {:nerves_system_rpi0, "~> 1.25", runtime: false, targets: :rpi0},
+      {:nerves_system_rpi3, "~> 1.25", runtime: false, targets: :rpi3},
+      {:nerves_system_rpi4, "~> 1.25", runtime: false, targets: :rpi4},
       {:picam, "~> 0.4.1"},
       {:ring_logger, "~> 0.8.1"},
       {:toolshed, "~> 0.2.13"},
