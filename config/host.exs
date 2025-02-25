7daef1830cdef94c6013f6a80017607cec1553c9
Joe Yates <joe.g.yates@gmail.com>
2025-02-25 14:26:55 +0100

    Reduce default logging level

--- config/host.exs
+++ config/host.exs
@@ -2,4 +2,6 @@ import Config
 
 # Configuration when running on the host.
 
-config :logger, :default_formatter, format: "$date $time $metadata[$level] $message\n"
+config :logger, :default_formatter,
+  format: "$date $time $metadata[$level] $message\n",
+  level: :info
