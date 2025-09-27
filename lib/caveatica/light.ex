bdd74b9840df72e5c8824a2fb869fa1fe42e5295
Joe Yates <joe.g.yates@gmail.com>
2025-09-27 16:23:12 +0200

    Use labels

--- lib/caveatica/light.ex
+++ lib/caveatica/light.ex
@@ -3,23 +3,22 @@ defmodule Caveatica.Light do
 
   require Logger
 
-  # GPIO23 == pin 16
-  @light_pin 23
+  @light_label "GPIO23"
 
   def turn_on() do
-    {:ok, gpio} = Circuits.GPIO.open(@light_pin, :output)
-    Circuits.GPIO.write(gpio, 1)
+    {:ok, gpio} = Circuits.GPIO.open(@light_label, :output)
+    Circuits.GPIO.write(@light_label, 1)
     Circuits.GPIO.close(gpio)
   end
 
   def turn_off() do
-    {:ok, gpio} = Circuits.GPIO.open(@light_pin, :output)
+    {:ok, gpio} = Circuits.GPIO.open(@light_label, :output)
     Circuits.GPIO.write(gpio, 0)
     Circuits.GPIO.close(gpio)
   end
 
   def status() do
-    {:ok, gpio} = Circuits.GPIO.open(@light_pin, :input)
+    {:ok, gpio} = Circuits.GPIO.open(@light_label, :input)
     value = Circuits.GPIO.read(gpio)
     Logger.info("light pin value: #{inspect(value)}")
     Circuits.GPIO.close(gpio)
